26151a6252befbdacda2f3f4100837030602037e
Ankita-328 <1974.ankita@gmail.com>
2025-01-14 22:39:52 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() =>{
-    await mongoose.connect('mongodb+srv://1974ankita:7iHn5AoGkC9e3A6o@cluster0.3xcpu.mongodb.net/foodCenter').then(()=>console.log("DB connected"))
+    await mongoose.connect('')
 }
